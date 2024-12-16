8b82b277b78024dd7c79fd2129ec41e32bea7074
Abhinav-Rusia <abhinavrusia@gmail.com>
2024-12-16 18:40:20 +0530

    different_component

---
+++ src/IngredientsList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const IngredientsList = (props) => {
+    const ingredientsListItems = props.ingredients.map((ingredient, index) => (
+        <li className="ingredient-item" key={index}>
+          {ingredient}
+        </li>
+      ));
+  return (
+    <section>
+          <div className="ingredient-container">
+            <h2>Ingredients on hand:</h2>
+            <ul className="ingredients-list" aria-live="polite">
+              {ingredientsListItems}
+            </ul>
+          </div>
+
+          {props.ingredients.length > 3 && (
+            <div className="get-recipe-container">
+              <div>
+                <h3>Ready for a recepie?</h3>
+                <p>Generate a recepie from your list of ingredients.</p>
+              </div>
+              <button onClick={props.toggleRecepie}>Get a recepie</button>
+            </div>
+          )}
+        </section>
+  )
+}
+
+export default IngredientsList
--- src/MainSection.jsx
+++ src/MainSection.jsx
@@ -1,13 +1,12 @@
 import React from "react";
+import ClaudeRecipe from "./ClaudeRecipe";
+import IngredientsList from "./IngredientsList";
+
 
 export default function MainSection() {
   const [ingredients, setIngredients] = React.useState([]);
 
-  const ingredientsListItems = ingredients.map((ingredient, index) => (
-    <li className="ingredient-item" key={index}>
-      {ingredient}
-    </li>
-  ));
+  const [recepieShown , setRecepieShown] = React.useState(false)
 
   function addIngredient(event) {
     event.preventDefault();
@@ -18,6 +17,10 @@ export default function MainSection() {
       event.target.reset();
     }
   }
+
+  function toggleRecepie(){
+    setRecepieShown((prevState)=> !prevState)
+}
 
   return (
     <main>
@@ -31,26 +34,20 @@ export default function MainSection() {
         />
         <button type="submit">Add ingredient</button>
       </form>
-      {ingredients.length > 0 && (
-        <section>
-          <div className="ingredient-container">
-            <h2>Ingredients on hand:</h2>
-            <ul className="ingredients-list" aria-live="polite">
-              {ingredientsListItems}
-            </ul>
-          </div>
+      {ingredients.length > 0 && <IngredientsList
+      toggleRecepie = {toggleRecepie}
+      ingredients = {ingredients}
+      />}
 
-          {ingredients.length > 3 && (
-            <div className="get-recipe-container">
-              <div>
-                <h3>Ready for a recepie?</h3>
-                <p>Generate a recepie from your list of ingredients.</p>
-              </div>
-              <button>Get a recepie</button>
-            </div>
-          )}
-        </section>
-      )}
+      {recepieShown ? <ClaudeRecipe/> : null }
     </main>
   );
 }
+
+
+
+
+
+
+
+
